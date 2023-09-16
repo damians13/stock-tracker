d92c9f9ad492905f5364908d1c4671732f390e97
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-09-16 09:39:53 -0700

    Install typescript and dotenv

---
+++ system/src/index.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv"
+
+dotenv.config({ path: __dirname + "\\..\\.env" })
+
+console.log(process.env.POSTGRES_USER)
+
+// const sql = postgres(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@0.0.0.0:5432/postgres`)
+
+// async function test() {
+// 	return await sql`SELECT * FROM Account`
+// }
+
+// console.log(test())

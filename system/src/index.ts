01901ddc8bc77cf1e52179dd7a164d89fc0e4a65
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-09-19 22:24:45 -0700

    Connected node to postgres

--- system/src/index.ts
+++ system/src/index.ts
@@ -1,15 +1,49 @@
 import * as dotenv from "dotenv"
 import express from "express"
+import { Client } from "pg"
 
 dotenv.config({ path: __dirname + "/../.env" })
 
 const app = express()
 const port = 8000
 
-app.get("/", (req, res) => {
+const pg = new Client({
+	host: "db",
+	port: 5432,
+	database: process.env.PGDATABASE,
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+})
+
+app.get("/", async (req, res) => {
 	res.send("Hello World!")
+
+	await pg.connect()
+	console.log("Connected to database")
+	let result = await pg.query("SELECT * FROM Account")
+	console.log(result)
+	console.log("Disconnected from database")
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+	console.log(`System server listening on port ${port}`)
+})
+
+async function cleanup(exitCode: number | undefined = 0) {
+	// Close the connection to the database
+	await pg.end()
+	process.exit(exitCode)
+}
+
+process.on("exit", async exitCode => {
+	cleanup()
+})
+process.on("SIGINT", async exitCode => {
+	cleanup()
+})
+process.on("SIGABRT", async exitCode => {
+	cleanup()
+})
+process.on("SIGTERM", async exitCode => {
+	cleanup()
 })

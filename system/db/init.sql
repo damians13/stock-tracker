3311e1089028a3c104f7b79c77daec136cbd4137
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-09-28 20:19:55 -0700

    Add some of the new tables

--- system/db/init.sql
+++ system/db/init.sql
@@ -1,7 +1,7 @@
 /* Create the tables */
 
 CREATE TABLE IF NOT EXISTS account (
-	id SERIAL PRIMARY KEY,
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name TEXT NOT NULL,
 	email TEXT NOT NULL UNIQUE,
 	password_hash CHAR(60) NOT NULL,
@@ -9,10 +9,53 @@ CREATE TABLE IF NOT EXISTS account (
 );
 
 CREATE TABLE IF NOT EXISTS portfolio (
-	id SERIAL NOT NULL,
+	account_id INTEGER,
 	name TEXT NOT NULL,
-	PRIMARY KEY (id, name),
-	FOREIGN KEY (id) REFERENCES account
+	PRIMARY KEY (account_id, name),
+	FOREIGN KEY (account_id) REFERENCES account ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS auth_session (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	account_id INTEGER,
+	created_date DATE,
+	created_time TIME,
+	is_active BOOLEAN,
+	FOREIGN KEY (account_id) REFERENCES account ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS stock (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	ticker TEXT NOT NULL,
+	exchange TEXT NOT NULL,
+	company_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS historical_stock_price (
+	stock_id INTEGER,
+	created_date DATE,
+	created_time TIME,
+	open_price INTEGER,
+	close_price INTEGER,
+	high_price INTEGER,
+	low_price INTEGER,
+	volume_traded INTEGER,
+	PRIMARY KEY (stock_id, created_date, created_time),
+	FOREIGN KEY (stock_id) REFERENCES stock ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS transaction (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+	/* TODO */
+);
+
+CREATE TABLE IF NOT EXISTS held_share (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	fraction DECIMAL NOT NULL CHECK (fraction > 0 AND fraction <= 1),
+	purchase_transaction_id INTEGER NOT NULL,
+	sale_transaction_id INTEGER,
+	FOREIGN KEY (purchase_transaction_id) REFERENCES transaction ON DELETE CASCADE,
+	FOREIGN KEY (sale_transaction_id) REFERENCES transaction ON DELETE CASCADE
 );
 
 /* Insert test data */

4478540398fa86898f32b2db842f839ff6f68de6
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-10-03 21:32:42 -0700

    Add logging for duplicate registration events

--- system/src/api/register.ts
+++ system/src/api/register.ts
@@ -2,17 +2,21 @@ import { Router } from "express"
 import db from "../util/db.js"
 import bcrypt from "bcrypt"
 import { AccountEventType, logAccountEvent } from "../util/log.js"
-import { getDateTime } from "../util/dateTime.js"
+import { DateTime, getDateTime } from "../util/dateTime.js"
 
 const router = Router()
 
 router.post("/", async (req, res) => {
-	let queryResponse = await db.query(`SELECT * FROM account WHERE EMAIL = '${req.body.email}'`)
+	let queryResponse = await db.query(`SELECT id FROM account WHERE EMAIL = '${req.body.email}'`)
 
 	if (queryResponse.rowCount !== 0) {
-		// There is already a user registered with that email
+		// There is already a user registered with that email, log the registration attempt
+		let accountId: Number = queryResponse.rows[0].id
+		await handleDuplicateRegistrationEvent(accountId)
+
 		res.status(409)
 		res.send("There is already a user registered with that email address.")
+
 		return
 	}
 
@@ -30,6 +34,24 @@ router.post("/", async (req, res) => {
 	res.status(201)
 	res.send("Account created.")
 })
+
+async function createInactiveSession(accountId: Number, dateTime: DateTime = getDateTime()): Promise<Number> {
+	let authSessionInsertResult = await db.query(`INSERT INTO auth_session (
+		account_id,
+		created_date,
+		created_time,
+		is_active
+	) VALUES (
+		${accountId},
+		'${dateTime.dateString}',
+		'${dateTime.timeString}',
+		FALSE
+	) RETURNING id`)
+
+	let authSessionId: Number = authSessionInsertResult.rows[0].id
+
+	return authSessionId
+}
 
 async function registerNewUser(name: String, email: String, passwordHash: String, passwordSalt: String) {
 	let dateTime = getDateTime()
@@ -49,21 +71,20 @@ async function registerNewUser(name: String, email: String, passwordHash: String
 	let accountId: Number = accountInsertResult.rows[0].id
 
 	// Create an inactive auth session for the account (to log the registration event)
-	let authSessionInsertResult = await db.query(`INSERT INTO auth_session (
-		account_id,
-		created_date,
-		created_time,
-		is_active
-	) VALUES (
-		${accountId},
-		'${dateTime.dateString}',
-		'${dateTime.timeString}',
-		FALSE
-	) RETURNING id`)
-	let authSessionId: Number = authSessionInsertResult.rows[0].id
+	let authSessionId = await createInactiveSession(accountId, dateTime)
 
 	// Log the registration event in the database
 	await logAccountEvent(db, authSessionId, AccountEventType.REGISTER, dateTime)
+}
+
+async function handleDuplicateRegistrationEvent(accountId: Number) {
+	let dateTime = getDateTime()
+
+	// Create an inactive auth session for the account (to log the duplicate registration attempt)
+	let authSessionId = await createInactiveSession(accountId, dateTime)
+
+	// Log the duplicate registration attempt in the database
+	await logAccountEvent(db, authSessionId, AccountEventType.DUPLICATE_REGISTER_ATTEMPT, dateTime)
 }
 
 export default router

a34047212440a7977105ec5979585caec900e9b4
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-10-21 11:59:37 -0700

    Add portfolio logging util method

--- system/src/util/log.ts
+++ system/src/util/log.ts
@@ -12,6 +12,12 @@ export enum AccountEventType {
 	DETELE_PORTFOLIO = "DELETE_PORTFOLIO",
 }
 
+export enum PortfolioEventType {
+	NEW = "NEW",
+	DELETE = "DELETE",
+	RENAME = "RENAME",
+}
+
 /**
  * Log information about an account to the database
  * @param db Database client connection
@@ -48,6 +54,49 @@ export async function logAccountEvent(db: Client, authSessionId: number, eventTy
 	) VALUES (
 		${logId},
 		${accountId},
+		'${eventType}'
+	)`)
+
+	return logId
+}
+
+export async function logPortfolioEvent(
+	db: Client,
+	authSessionId: number,
+	portfolioName: string,
+	eventType: PortfolioEventType,
+	dateTime: DateTime = getDateTime(),
+	message?: string
+): Promise<number> {
+	let accountQueryResult = await db.query(`SELECT account_id FROM auth_session WHERE id = ${authSessionId}`)
+
+	// Include the message as the log_message if it is given
+	let logInsertResult = await db.query(
+		`INSERT INTO log_event (
+			auth_session_id,
+			created_date,
+			created_time
+			${message !== undefined ? `, log_message` : ""}
+		) VALUES (
+			${authSessionId},
+			'${dateTime.dateString}',
+			'${dateTime.timeString}'
+			${message !== undefined ? ", '" + message + "'" : ""}
+		) RETURNING id`
+	)
+
+	let accountId: number = accountQueryResult.rows[0].account_id
+	let logId: number = logInsertResult.rows[0].id
+
+	await db.query(`INSERT INTO portfolio_log_event (
+		id,
+		account_id,
+		portfolio_name,
+		log_type
+	) VALUES (
+		${logId},
+		${accountId},
+		'${portfolioName}'
 		'${eventType}'
 	)`)
 

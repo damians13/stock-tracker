d9dbe85b09f3a97bba1a8143b2197fc6afae52a7
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-10-22 22:15:39 -0700

    Fix system account insert SQL

--- system/db/insert.sql
+++ system/db/insert.sql
@@ -1,16 +1,16 @@
-/* Insert test data */
+/* Create an account for the system (for logging) */
 INSERT INTO account (
 	id,
-	name,
+	client_name,
 	email,
 	password_hash,
 	password_salt
-) VALUES (
+) OVERRIDING SYSTEM VALUE VALUES (
 	0,
 	'system',
 	'system@gmail.com',
 	'$2b$10$V8DMEZF6gh3t/9F9OSL8luBNkXVFfT8YMo/MF02f2neiU4g2F0WTC',
 	'$2b$10$V8DMEZF6gh3t/9F9OSL8lu'
-) WHERE NOT EXISTS (
-	SELECT * FROM account WHERE id = 0
-);
+) ON CONFLICT DO NOTHING;
+
+/* Insert test data */
